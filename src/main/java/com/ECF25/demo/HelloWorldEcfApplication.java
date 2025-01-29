598e364aca3f46d0085a76ab115cd43467a0794c
LiquidCQC <84178150+LiquidCQC@users.noreply.github.com>
2025-01-29 16:49:17 +0100

    first commit

---
+++ src/main/java/com/ECF25/demo/HelloWorldEcfApplication.java
@@ -0,0 +1,13 @@
+package com.ECF25.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldEcfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldEcfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ECF25/demo/HelloWorldEcfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ECF25.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldEcfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
